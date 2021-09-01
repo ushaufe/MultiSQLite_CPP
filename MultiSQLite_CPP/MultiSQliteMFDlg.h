ca68a855f626d2b7ebc900eaa543c5b7b41854d3
Uwe <uwe.stahlschmidt@haufe-lexware.com>
2021-09-01 14:37:15 +0200

    Version 0.9.0.1 - Has been changed to statically bound MFC application again - Remaining items have been renamed to MultiSQLite_CPP

---
+++ MultiSQLite_CPP/MultiSQliteMFDlg.h
@@ -0,0 +1,176 @@
+
+// MultiSQliteMFDlg.h : header file
+//
+
+// This is a C++ project to demonstrage how well SQLite can handle Multithreading
+// The documentation for this project and the accompanying C# projects can be found here:
+// (the location may be moved by the webmaster of the wiki)
+// https://mywiki.grp.haufemg.com/pages/viewpage.action?pageId=156088657
+
+#pragma once
+#include "sqlite/sqlite3.h"
+#include "VersionInfo.h"
+
+
+class CFlickerObject : public CObject {
+
+};
+
+class CAboutDlg;
+
+// CMultiSQliteMFDlg dialog
+class CMultiSQliteMFDlg : public CDialogEx
+{
+// Construction
+public:
+	CMultiSQliteMFDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MAINDIALOG };
+#endif
+
+	static bool bFlickerLock1;
+	static bool bFlickerLock2;
+
+	static bool bPollock;
+
+	static long maxThreadID;
+
+	const int DB_LOCK_INTERVAL = 100;
+
+	const int DB_VERSION_MIN = 1000;
+	const int DB_VERSION_MAX = 2000;
+	const CString DB_VERSION_STR = L"2.0.0.0";
+
+	CMenu *menu;
+
+	protected:
+	// A handle to the ListBox must be kept static
+	// so that the different threads have access to the GUI
+	// This must be defined static so that the threads can access it outside of 
+	// the instance-contect
+	static CListBox* lb;
+	
+	
+	// This database-object is used for access without connection-pooling (multiple threads / one connection)
+	static sqlite3 *dbx;
+	CMutex mutexDB;
+
+	// Different instances of the the database are used
+	// For simultaneous multithreading with multiple connection-objects (=Pooling)
+	// see => OnBnClickedMulticonnectWrite()
+	static sqlite3 *db0;
+	static sqlite3 *db1;
+	static sqlite3 *db2;
+	// The instances must be static to be accessible
+	// from the threads and hence outside of the 
+	// context of the class
+	
+	CAboutDlg* aboutDlg;
+
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+	
+	
+	// A Window-handle must be kept 
+	// so that the different threads have access to the GUI
+	// This must be defined static so that the threads can access it outside of 
+	// the instance-contect
+	static CWnd* staticWnd;
+	bool bIsDebug = false;
+	CFlickerObject* pFlickerObject;
+	int nNumberThreads;
+
+	static CString CMultiSQliteMFDlg::RandomString(int len, int iType);
+
+
+	static UINT ThreadSQLHammerIn(LPVOID pParam);	
+
+	static UINT ThreadSQLFlicker1(LPVOID pParam);
+	static UINT ThreadSQLFlicker2(LPVOID pParam);
+
+	static UINT ThreadSQLMultiHammerIn1(LPVOID pParam);
+	static UINT ThreadSQLMultiHammerIn2(LPVOID pParam);
+
+// Implementation
+protected:
+	HICON m_hIcon;
+	
+	void ShowNumberOfThreads();
+
+	// This method is called automatically after a "select from" database operation
+	static int callback(void *NotUsed, int argc, char **argv, char **azColName);
+
+
+	static int UpdateApplications(void* NotUsed, int argc, char** argv, char** azColName);
+
+	// This method is called automatically after a "select from" database operation
+	// It fills the listbox-object and is called simultaneously from multiple threads
+	static int callback_flicker(void *NotUsed, int argc, char **argv, char **azColName);
+
+	bool UpdateApp();
+	CString getAppData();
+	bool PeekAndPump();
+	
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+	
+	void Connect();
+	void Disconnect();
+	boolean execQuery(CString strQuery);
+	void UpdateMenuItems();
+	boolean setAppID();
+	bool getSQLInt(CString strSQL,int &nInt);
+	bool getDBVersion(CString& strDBVersion);
+	int getDBVersionNumber(CString strDBVersion);
+	CString getDBVersionString(int nDBVersion);
+	int getDBVersionNumber();
+	CString strDatabaseFile;
+	static CString strAppID;
+	UINT_PTR tiUpdateApps = 1;
+	UINT_PTR tiRestartAfterUpdate = 2;
+	UINT_PTR tiCloseAfterUpdate = 3;
+	UINT_PTR tiStartupDelay = 4;
+
+	const CString DB_NAME = L"MultiSQLite.db";
+	const CString APP_NAME = L"Haufe Multi - SQlite for C++";
+	CString APP_DB_NAME = L"MultiSQlite_CPP";
+	std::string ProcessIdToName(DWORD processId);
+	const int DB_REVISION = 2;
+
+public:
+	afx_msg void OnBnClickedBtnConnect();
+	afx_msg void OnLbnSelchangeList();		
+	
+	afx_msg void OnBnClickedHammerinsql();
+	afx_msg void OnBnClickedShowdbcontents();
+	afx_msg void OnBnClickedShowDbCountSingle();
+	afx_msg void OnBnClickedHammer2sql();
+	//afx_msg void OnBnClickedButton7();
+	afx_msg void OnBnClickedMulticonnectWrite();
+	afx_msg void OnBnClickedSqlmultihammer();
+	afx_msg void OnBnClickedMulticountOnce();
+	afx_msg void OnBnClickedSingleinsert();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnClose();
+	afx_msg void OnBnClickedStartthreadssinglecon();
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnBnClickedFlickerCount();
+	afx_msg void OnBnClickedUpdate();
+	afx_msg void OnFileExit();	
+	afx_msg void OnFileDisconnect();	
+	afx_msg void OnUpdateWindowNew(CCmdUI* pCmdUI);
+	afx_msg void OnHelpAbout();
+	afx_msg void OnHelpUpdate();
+	afx_msg void OnActionsStartthreads();
+	afx_msg void OnActionsStopthreads();
+	afx_msg void OnFileConnect();
+};
+
+
+
+
---
+++ MultiSQLite_CPP/resource.h
@@ -0,0 +1,61 @@
+//{{NO_DEPENDENCIES}}
+// Von Microsoft Visual C++ generierte Includedatei.
+// Verwendet durch MultiSQliteMF.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDR_MAINFRAME                   131
+#define IDB_HAUFE                       136
+#define IDB_HAUFE16                     138
+#define IDD_MAINDIALOG                  141
+#define IDB_SQLITE                      147
+#define IDB_APP_TITLE                   148
+#define IDB_CPP                         149
+#define IDB_CPP32                       150
+#define IDB_CPP48                       151
+#define IDB_BACKGROUND2                 160
+#define IDI_ICON1                       163
+#define IDR_MENU                        164
+#define IDC_BTN_CONNECT                 1000
+#define IDC_LIST                        1001
+#define IDC_Multithread                 1002
+#define BTN_MFC_MULTITHREAD             1003
+#define BTN_Connect4SQLite              1005
+#define BTN_SingleInsert                1005
+#define BTN_HammerInSQL                 1006
+#define BTN_ShowDBContents              1007
+#define IDC_SHOW_DB_COUNT_SINGLE        1008
+#define BTN_Hammer2SQL                  1009
+#define BTN_MultiConnect_Write          1010
+#define BTN_SQLMultiHammer              1011
+#define BTN_MultiCount_Once             1012
+#define DB_Flicker_Counn                1013
+#define BTN_FlickerCount                1013
+#define BTN_FLICKER_COUNT               1013
+#define IDC_CPP_PIC                     1020
+#define IDLB_APPLICATIONS               1026
+#define IDC_VERSION                     1027
+#define BTN_StartThreadsSingleCon       1029
+#define IDC_SLIDER_NUMBER_THREADS       1033
+#define IDC_LBL_NUMBER_THREADS          1034
+#define IDC_BUTTON2                     1037
+#define BTN_Update                      1037
+#define ID_FILE_EXIT                    32771
+#define ID_FILE_CONNECT                 32772
+#define ID_FILE_DISCONNECT              32773
+#define ID_HELP_ABOUT                   32774
+#define ID_HELP_UPDATE                  32775
+#define ID_ACTIONS_STARTTHREADS         32776
+#define ID_ACTIONS_STOPTHREADS          32777
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        166
+#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_CONTROL_VALUE         1039
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
